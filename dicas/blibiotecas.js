9e8d98286fee8207c6d09e5eea226839de7d33dd
Adeilson Nogueira <adeilson.n.macedo@gmail.com>
2023-03-03 13:51:25 -0300

    atualizado dicas

--- dicas/blibiotecas.js
+++ dicas/blibiotecas.js
@@ -33,3 +33,36 @@ git push -u origin main
 */
 
 git config --list //ver as configuraçoes do git
+//@ como mudar o usuário git da maquina https://coffops.com/alteracao-de-usuario-git/
+git config --global user.name "Luiz Antonio Rech"
+git config --global user.email luiz.rech@conffops.com.br
+//para trocar o usuario precisa remove-lo das credenciais do windows
+//Branch permite que muitas pessoas trabalhem em um unico projeto  -> Tarefa
+#Branch (Tarefa)
+git checkout -b "nova tarefa" //cria uma nova tarefa
+//adiciona um commit 
+git push --set-upstream origin nova_tarefa //cria uma nova tarefa para trabalhar
+//mudar para tarefa main
+git checkout main
+//ver em qual branch 
+git branch //main se for o caso , mosta o branch
+
+//junta os projetos (tarefas)
+git merge main //(main estando na raiz) 
+git push //para mandar para a git hub
+
+
+//#Atualizar o projeto se atentar ao bratch trabalhado
+git pull
+git clone 'https://github.com/Adeilsonnm/ajax_testes.git' nome_da_pasta //clona o projeto e adiciona a pasta
+//possivel dar modificar e commitar o main, e mudar o bratch durante o processo
+
+git clean -f //remove aquivos adicionados por engano
+
+
+//@dica desfazer 
+git checkout './filmes.php' //funciona como um ctrol-z , desfaz
+
+git log //movimentaçoes do projeto
+
+git branch -d 'branch' //aqui é possivel escluir um branch
